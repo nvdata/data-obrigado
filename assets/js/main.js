b6617c7cee5c9c678f4fdf3ce68a58802ca4794d
adr <senhoraroldo@gmail.com>
2021-06-15 11:36:35 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/*
+// import vemBootstrap from './modules/botstrapimport.js';
+// import * as animacao from './modules/animation.js';
+// import * as carouselClick from './modules/carousel-change.js';
+// import linkDentroLink from './modules/a-dentro-a.js'; //js do menu
+// import initAccordion from "./modules/accordion.js"
+
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js';
+import * as config from './assets/js/modules/modules/swiper__props.js'; //Configs do Swiper
+
+
+// linkDentroLink()
+// linkDentroLink()
+menu();
+
+*/
+
+
